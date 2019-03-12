c55462bac4366634e99888d84120f8b85768ef5b
ombaez <ombaez5689@gmail.com>
2019-03-12 19:39:36 -0300

    check

--- front/src/index.js
+++ front/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import Main from '../src/components/Main';
+import Main from '../src/containers/Main';
 import store from './redux/store';
 
 ReactDOM.render(
