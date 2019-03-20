94052442c95db0a9d00a32d214a812b2e8624462
amatana <amatana95@gmail.com>
2019-03-20 15:32:55 -0300

    empecé las rel de carrito con productos

--- back/models/Carrito.js
+++ back/models/Carrito.js
@@ -1,15 +1,14 @@
 const S = require('sequelize');
-const db = require ('../config/db')
-
+const db = require('../config/db');
 
 const Carrito = db.define('carrito', {
-    cantidad: {
-        type:S.INTEGER,
-        allowNull: false,
-    }
-})
+  cantidad: {
+    type: S.INTEGER,
+    allowNull: false
+  }
+});
 // cantidad de product id
-// User belong to many 
+// User belong to many
+Carrito.belongsTo(Carrito, { as: 'owner' });
 
-
-module.exports = Carrito
+module.exports = Carrito;
--- back/models/index.js
+++ back/models/index.js
@@ -1,5 +1,6 @@
 const Productos = require('./Producto');
 const Categorias = require('./Categorias');
+const Carrito = require('./Carrito');
 
 const modelos = {
   Categorias,
@@ -9,6 +10,15 @@ const modelos = {
 Categorias.belongsToMany(Productos, { through: 'categorias_productos' });
 Productos.belongsToMany(Categorias, { through: 'categorias_productos' });
 
+const carrito = {
+  Carrito,
+  Productos
+};
+
+Carrito.belongsToMany(Productos, { through: 'carrito_productos' });
+Productos.belongsToMany(Carrito, { through: 'productos_carritos'});
+
 module.exports = {
-  modelos
+  modelos,
+  carrito
 };
